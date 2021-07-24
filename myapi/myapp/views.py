6f5acad752cd2f807f4b8a71c09b733b0b27dd5a
Rafael <rafael.quelho@gmail.com>
2021-07-24 18:55:50 -0300

    Updating views

--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -1,8 +1,48 @@
 from rest_framework import generics
-from myapp.serializers.music_serializer import MusicSerializer
-from myapp.models.music import Music
+from myapp.serializers import (
+    MusicSerializer,
+    AlbumSerializer,
+    BandSerializer,
+    MemberSerializer,
+)
+from myapp.models import Music, Album, Band, Member
 
 
 class MusicList(generics.ListCreateAPIView):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
+
+
+class MusicDetail(generics.RetrieveDestroyAPIView):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+
+
+class AlbumList(generics.ListCreateAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+
+class AlbumDetail(generics.RetrieveDestroyAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+
+class BandList(generics.ListCreateAPIView):
+    queryset = Band.objects.all()
+    serializer_class = BandSerializer
+
+
+class BandDetail(generics.RetrieveDestroyAPIView):
+    queryset = Band.objects.all()
+    serializer_class = BandSerializer
+
+
+class MemberList(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+class MemberDetail(generics.RetrieveDestroyAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
