30d5d65dca9f1dded0775176e2703989c3c7eb5c
Rafael <rafael.quelho@gmail.com>
2021-07-24 17:04:33 -0300

    Creating serializer and view for music

---
+++ myapi/myapp/serializers.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Music
+
+
+class MusicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Music
+        fields = "__all__"
--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import MusicSerializer
+from .models import Music
 
-# Create your views here.
+
+class MusicList(generics.ListCreateAPIView):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
